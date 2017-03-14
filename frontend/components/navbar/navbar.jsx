4c40dfce4276bc906a0c68bd1736b899f6b34d5b
Michael Cittar <mcittar@gmail.com>
2017-03-14 14:57:11 -0700

    Start heroku configuration

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -21,22 +21,18 @@ class NavBar extends React.Component {
       );
     } else {
       rightNav = (
-        <li>
-          <Link to='/signup'>
-            Sign Up
-          </Link>
-          <Link to='/login'>
-            Log In
-          </Link>
-        </li>
+        <ul>
+          <li><Link to='/signup'>Sign Up</Link></li>
+          <li><Link to='/login'>Log In</Link></li>
+        </ul>
       );
     }
 
     return (
       <ul>
         <li><Link to='/discover'>Discover</Link></li>
-        <li><h3>GetRichQuickStarter</h3></li>
-        { rightNav }
+        <li><Link to='/'><h3>GetRichQuickStarter</h3></Link></li>
+        <li>{ rightNav }</li>
       </ul>
     );
   }
