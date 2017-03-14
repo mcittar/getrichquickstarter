bdd502f79ab8af8d18a4177b274fc63090b2fdd5
Michael Cittar <mcittar@gmail.com>
2017-03-14 13:40:25 -0700

    Add Root and Provider to component tree

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>GetRichQuickStarter</h1>
+    { children }
+  </div>
+);
+
+export default App;
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _nullUser = {
+  currentUser: null,
+  errors: []
+};
+
+const SessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, _nullUser, { currentUser });
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, _nullUser, { errors });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
